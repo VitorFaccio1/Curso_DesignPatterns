a49c3714e08a5618dd5774cdfd1b75940857933b
Vitor Faccio <vitorfaccio6@gmail.com>
2024-09-04 22:15:48 -0300

    Adiciona Iterator

---
+++ DesignPatterns/Behavioral Patterns/Iterator/Interfaces/IIterator.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns.Behavioral_Patterns.Iterator.Interfaces;
+
+public interface IIterator
+{
+    bool HasNext();
+
+    object Next();
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DesignPatterns.Behavioral_Patterns.Command;
 using DesignPatterns.Behavioral_Patterns.Interpreter.Calculator;
+using DesignPatterns.Behavioral_Patterns.Iterator;
 using DesignPatterns.Behavioral_Patterns.Observer;
 using DesignPatterns.Behavioral_Patterns.Strategy;
 using DesignPatterns.Behavioral_Patterns.Template_Method;
@@ -38,7 +39,8 @@ while (designPatternOption != 0)
         "\n12)Interpreter" +
         "\n13)Observer" +
         "\n14)Command" +
-        "\n15)Strategy");
+        "\n15)Strategy" +
+        "\n17)Iterator");
 
     Console.Write("\nDigite a opção que deseja: ");
     designPatternOption = Convert.ToInt32(Console.ReadLine());
@@ -92,6 +94,9 @@ while (designPatternOption != 0)
             break;
         case 15:
             new StrategyClient().Menu();
+            break;
+        case 17:
+            new IteratorClient().Menu();
             break;
         default:
             break;
